96bd14440b3a6c123b54ba70d11e691416b36976
Petrando <petrandorichard@gmail.com>
2024-11-25 11:25:44 +0800

    establish colors in tailwind.config.js

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
 
   return (
     <main>
-      <h1 className='font-bold text-lg font-zentry'>Vite + React</h1>
+      <h1 className='font-bold text-lg font-zentry text-yellow-300'>Vite + React</h1>
     </main>
   )
 }
