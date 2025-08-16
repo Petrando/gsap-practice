9bd874fd296c1cea6f33312ad467f12d410c3d21
Petrando <petrandorichard@gmail.com>
2025-08-16 15:19:21 +0800

    Story section with light and dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -47,6 +47,7 @@ function App() {
       <Hero />
       <About />
       <Features />
+      <Story />
     </div>
   );
 }
--- src/components/Story.tsx
+++ src/components/Story.tsx
@@ -46,7 +46,7 @@ const Story = () => {
         }
     };
     return (
-        <div id="story" className="min-h-dvh w-screen bg-black text-blue-50">
+        <div id="story" className="min-h-dvh w-screen bg-blue-50 text-slate-900 dark:bg-black dark:text-blue-50">
             <div className="flex size-full flex-col items-center py-10 pb-24">
                 <p className="font-general text-sm uppercase md:text-[10px]">
                     the multiversal ip world
@@ -55,7 +55,7 @@ const Story = () => {
                 <div className="relative size-full">
                     <AnimatedTitle
                         title="the st<b>o</b>ry of <br /> a hidden real<b>m</b>"
-                        containerClass="mt-5 pointer-events-none mix-blend-difference relative z-10"
+                        containerClass="mt-5 pointer-events-none mix-blend-difference relative z-10 text-slate-900 dark:text-blue-50"
                     />
 
                     <div className="story-img-container">
@@ -80,7 +80,7 @@ const Story = () => {
 
                 <div className="-mt-80 flex w-full justify-center md:-mt-64 md:me-44 md:justify-end">
                     <div className="flex h-full w-fit flex-col items-center md:items-start">
-                        <p className="mt-3 max-w-sm text-center font-circular-web text-violet-50 md:text-start">
+                        <p className="mt-3 max-w-sm text-center font-circular-web text-violet-950 dark:text-violet-50 md:text-start">
                             Where realms converge, lies Zentry and the boundless pillar.
                             Discover its secrets and shape your fate amidst infinite
                             opportunities.
