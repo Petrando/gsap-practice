e96410fb4b898b2949c8f2a47fd87c2087b9b8d2
Petrando <petrandorichard@gmail.com>
2025-08-16 14:52:13 +0800

    Features section with light and dark mode

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -61,7 +61,7 @@ export const BentoCard:FC<IBento> = ({ src, title, description }) => {
                 autoPlay
                 className="absolute left-0 top-0 size-full object-cover object-center"
             />
-            <div className="relative z-10 flex size-full flex-col justify-between p-5 text-blue-50">
+            <div className="relative z-10 flex size-full flex-col justify-between p-5 text-yellow-200 dark:text-blue-50">
                 <div>
                 <h1 className="bento-title special-font">{title}</h1>
                 {description && (
@@ -113,13 +113,13 @@ const Features = () => {
     ]
 
     return (
-        <section className="bg-black pb-52">
+        <section className="bg-white dark:bg-black pb-52">
             <div className="container mx-auto px-3 md:px-10">
                 <div className="px-5 py-32">
-                    <p className="font-circular-web text-lg text-blue-50">
+                    <p className="font-circular-web text-lg text-blue-900 dark:text-blue-50">
                         Into the Metagame Layer
                     </p>
-                    <p className="max-w-md font-circular-web text-lg text-blue-50 opacity-50">
+                    <p className="max-w-md font-circular-web text-lg text-blue-800 dark:text-blue-50 opacity-50">
                         Immerse yourself in a rich and ever-expanding universe where a vibrant
                         array of products converge into an interconnected overlay experience
                         on your world.
@@ -144,8 +144,8 @@ const Features = () => {
                         {...bentos[3]} />                    
 
                     <BentoTilt className="bento-tilt_2">
-                        <div className="flex size-full flex-col justify-between bg-violet-300 p-5">
-                            <h1 className="bento-title special-font max-w-64 text-black">
+                        <div className="flex size-full flex-col justify-between bg-violet-300 dark:bg-violet-950 p-5">
+                            <h1 className="bento-title special-font max-w-64 text-black dark:text-violet-200">
                             M<b>o</b>re co<b>m</b>ing s<b>o</b>on.
                             </h1>
 
